0266f75a1d809172a8761733a69bc05544a47f6d
AreAtomic <aureliensebe@gmail.com>
2023-01-09 10:23:20 +0100

    Use config module correctly

--- config/local.js
+++ config/local.js
@@ -2,6 +2,9 @@
 
 let localConfig = {
     port: 5000,
+    mongoURI:
+        'mongodb+srv://areatomic:qJr5uoKFlX5thgtN@areatomic.ms0nffq.mongodb.net/areatomic?retryWrites=true&w=majority',
+    jwtSecret: 'yourstrongsecrettokenmustbechanged',
 }
 
 module.exports = localConfig
